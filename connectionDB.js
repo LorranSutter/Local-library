048ca1fc047cb4878133bc2a4d99e57d1f5b8e98
Lorran Sutter <sutterlorran@gmail.com>
2020-05-17 18:58:30 -0400

    Update connectionDB.js

--- connectionDB.js
+++ connectionDB.js
@@ -1,13 +1,25 @@
 //Import the mongoose module
 const mongoose = require('mongoose');
 
-//Set up default mongoose connection
-const dev_db_url = 'DATABASE-URL';
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true, useFindAndModify: true });
+const MONGOURI = "DATABASE-URL";
+const mongoDB = process.env.MONGODB_URI || MONGOURI;
 
-//Get the default connection
-const db = mongoose.connection;
+const InitiateMongoServer = async () => {
+    try {
+        await mongoose.connect(mongoDB, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log("Connected to DB !!");
 
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+        const db = mongoose.connection;
+
+        //Bind connection to error event (to get notification of connection errors)
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
+};
+
+module.exports = InitiateMongoServer;
