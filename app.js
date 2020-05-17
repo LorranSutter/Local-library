ec7f453a83a151a91e70ae063e6c336c7848237f
Lorran Sutter <sutterlorran@gmail.com>
2020-05-17 18:58:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,10 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 
-//Import the mongoose module
-const mongoose = require('mongoose');
+const InitiateMongoServer = require('./connectionDB');
 
-//Set up default mongoose connection
-const dev_db_url = 'mongodb+srv://lorran:BCDV1007@cluster0-lyl06.gcp.mongodb.net/local_library?retryWrites=true&w=majority';
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
-mongoose.connect(mongoDB, { useNewUrlParser: true });
-
-//Get the default connection
-const db = mongoose.connection;
-
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// Initiate Mongo Server
+InitiateMongoServer();
 
 const app = express();
 
